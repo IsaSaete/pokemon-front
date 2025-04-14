df095d34832ad506d3149a4f857562db042c6ea8
Isabel Sáenz <isasaete@gmail.comn>
2025-04-14 18:21:27 +0200

    Change the font and added the header media query

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@fontsource-variable/orbitron/index.css";
+import "@fontsource/slackey/index.css";
 import App from "./components/App/App";
 import "./styles/styles.css";
 
