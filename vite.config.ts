5776aa76b58a30028aafce2664dfc6ba05e7402a
Isabel Sáenz <isasaete@gmail.comn>
2025-04-14 17:49:19 +0200

    Add test Header component

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    root: "src",
+    globals: true,
+    reporters: ["verbose"],
+    environment: "jsdom",
+    setupFiles: ["./src/setupTest.ts"],
+  },
 });
