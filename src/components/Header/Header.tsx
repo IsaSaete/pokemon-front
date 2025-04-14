165f489c20dc2428d37eb92dcd6c22189040c080
Isabel Sáenz <isasaete@gmail.comn>
2025-04-14 17:47:07 +0200

    Add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <h1 className="app-title">pokedex</h1>
+    </header>
+  );
+};
+
+export default Header;
