eac025e02d79b52c59390a528b983cf0805745cc
Isabel Sáenz <isasaete@gmail.comn>
2025-04-15 19:07:58 +0200

    Add test to Navigation and modify Header & App tests with MemoryRouter

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
 import Header from "./Header";
 
 describe("Given the Header component", () => {
   describe("When it renders", () => {
-    test("Then it should show a title 'Pokedex' inside a heading", () => {
-      render(<Header />);
+    test("Then it should show a title 'Pokedex' inside a level 1 heading", () => {
+      render(
+        <MemoryRouter>
+          <Header />
+        </MemoryRouter>,
+      );
 
       const appTitle = screen.getByRole("heading", {
         name: /pokedex/i,
@@ -12,6 +17,18 @@ describe("Given the Header component", () => {
       });
 
       expect(appTitle).toBeVisible();
+    });
+
+    test("Then it should show a 'Pokémons' link", () => {
+      render(
+        <MemoryRouter>
+          <Header />
+        </MemoryRouter>,
+      );
+
+      const pokemonsLink = screen.getByRole("link", { name: /pokémons/i });
+
+      expect(pokemonsLink).toBeVisible();
     });
   });
 });
