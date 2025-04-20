a6e029ba59c2cf6d8782331d0e8510624f01b3b7
Isabel Sáenz <isasaete@gmail.comn>
2025-04-20 19:27:32 +0200

    Add createNewPokemon function to PokemonsContextProvider

--- src/pokemon/context/PokemonsContextProvider.tsx
+++ src/pokemon/context/PokemonsContextProvider.tsx
@@ -1,7 +1,7 @@
 import { PropsWithChildren, useCallback, useMemo, useState } from "react";
 import PokemonsContext from "./PokemonsContext";
 import { PokemonsContextStructure } from "./types";
-import { Pokemon } from "../types";
+import { Pokemon, PokemonFormData } from "../types";
 import PokemonClient from "../client/PokemonClient";
 
 const PokemonContextProvider: React.FC<PropsWithChildren> = ({ children }) => {
@@ -10,14 +10,25 @@ const PokemonContextProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const pokemonClient = useMemo(() => new PokemonClient(), []);
 
   const loadPokemons = useCallback(async (): Promise<void> => {
-    const apiPokemons = await pokemonClient.getPokemons();
+    const apiPokemons = await pokemonClient.getBackPokemons();
 
     setPokemons(apiPokemons);
   }, [pokemonClient]);
 
+  const createNewPokemon = async (
+    pokemonData: PokemonFormData,
+  ): Promise<Pokemon> => {
+    const newPokemon = await pokemonClient.addPokemon(pokemonData);
+
+    setPokemons((pokemons) => [...pokemons, newPokemon]);
+
+    return newPokemon;
+  };
+
   const pokemonsContexValue: PokemonsContextStructure = {
     pokemons,
     loadPokemons,
+    createNewPokemon,
   };
 
   return (
--- src/pokemon/pages/PokemonFormPage/PokemonFormPage.tsx
+++ src/pokemon/pages/PokemonFormPage/PokemonFormPage.tsx
@@ -1,11 +1,14 @@
 import PokemonForm from "../../components/PokemonForm/PokemonForm";
+import usePokemons from "../../hooks/usePokemons";
 import "./PokemonFormPage.css";
 
 const PokemonFormPage: React.FC = () => {
+  const { createNewPokemon } = usePokemons();
+
   return (
     <div className="main-content">
       <h2 className="title-page">Añade un Pokémon</h2>
-      <PokemonForm />
+      <PokemonForm action={createNewPokemon} />
     </div>
   );
 };
